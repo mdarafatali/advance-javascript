809795d40e5bd01e288004ee407fc70d1a82d311
MD ARAFAT ALI <76909049+mdarafatali@users.noreply.github.com>
2021-02-01 22:53:24 +0600

    advanceJs

---
+++ trueFalse.js
@@ -0,0 +1,16 @@
+//0 == false
+//"" == false
+//null == false
+//undefined == false
+//Nan == false
+//false == false
+
+let name = false;
+
+if (name){
+    console.log("The condition is True")
+}
+
+else{
+    console.log("The Condition is False")
+}
