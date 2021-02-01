1c8d1570208931c76e3a6a5f5c1e324a4576d317
MD ARAFAT ALI <76909049+mdarafatali@users.noreply.github.com>
2021-02-01 23:07:16 +0600

    null VS undefiend

---
+++ nullUndefiend.js
@@ -0,0 +1,10 @@
+// let pakhi;
+// console.log(pakhi);
+
+
+// function addNumber(num1, num2){
+//     console.log(num1+num2);
+// }
+//  const result = addNumber(20, 32);
+//  console.log(result);
+
